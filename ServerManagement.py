d1720fb9aa75cf1c735350b45095a3c93b7e0862
Miguel Sánchez Palomino <miguelsanchez1703@gmail.com>
2020-03-25 19:23:24 +0100

    Upload new ServerManagement.py

--- ServerManagement.py
+++ ServerManagement.py
@@ -5,26 +5,60 @@ from telegram.ext import CommandHandler
 from telegram.ext import Filters
 import subprocess
 
-#when /proves
-def proves(bot, update):
-	message = bot.send_message(chat_id=update.message.chat_id, text="Put your doubious stuff here")
-
 
 #when /awake
 def awake(bot, update):
 	message = bot.send_message(chat_id=update.message.chat_id, text="Checking...")
-	subprocess.call('./scripts/checkifawake.sh', shell=True) #check the awakeness of the server through the script
-	status = open('./resources/status.txt').read().strip() #read the output of the script
+	subprocess.call('absolute_path/scripts/checkifawake.sh', shell=True) #check the awakeness of the server through the script
+	status = open('absolute_path/resources/status.txt').read().strip() #read the output of the script
 	if status == 'awake':
 		bot.send_message(chat_id=update.message.chat_id, reply_to_message_id=message.message_id, text="Hello, I'm awake :)") #reply to the checking message
 	if status == 'asleep':
 		bot.send_message(chat_id=update.message.chat_id, reply_to_message_id=message.message_id, text="I'm sleeping :(") #reply to the checking message
 
 
+#find if there is a matching between the arguments and the list of series/movies
+def filter(bot, update, args):
+	bot.send_message(chat_id=update.message.chat_id, text="Inside the method")
+	if len(args) > 1:
+		shows = []
+		keyword = []
+		for k in range(1,len(args)):
+			keyword.append(args[k])
+		keyword = " ".join(keyword)
+		list = open('absolute_path/resources/list.txt')
+		for show in list:
+			if keyword.lower() in show.lower():
+				shows.append(show)
+		if len(shows) == 0:
+			bot.send_message(chat_id=update.message.chat_id, text="Nothing found :(")
+		else:
+			shows="- " + "- ".join(shows)
+			bot.send_message(chat_id = update.message.chat_id, text=shows)
+
+
+#when /list
+def list(bot, update, args):
+	if args[0] == "films":
+		subprocess.call('absolute_path/scripts/listfilms.sh', shell=True)
+		if len(args) == 1:
+			list = open('absolute_path/resources/list.txt').read().strip()
+			bot.send_message(chat_id=update.message.chat_id, text=list)
+		else:
+			filter(bot, update, args)
+	elif args[0] == "series":
+		subprocess.call('absolute_path/scripts/listseries.sh', shell=True)
+		if len(args) == 1:
+			list = open('absolute_path/resources/list.txt').read().strip()
+			bot.send_message(chat_id=update.message.chat_id, text=list)
+		else:
+			filter(bot, update, args)
+
+
 #when /getup
 def getup(bot, update):
 	message = bot.send_message(chat_id=update.message.chat_id, text="Can't I have 5 more mins... (Be a little patient)")
-	subprocess.call('./scripts/getup.sh', shell=True) #wakeonlan + ping until success through the script
+	subprocess.call('absolute_path/scripts/getup.sh', shell=True) #wakeonlan + ping until success through the script
 	bot.send_message(chat_id=update.message.chat_id, reply_to_message_id=message.message_id, text="Ready to rock!") #reply to the 'time to get up' message
 
 
@@ -38,10 +72,21 @@ def help(bot, update):
 	+ "/wakeup - turns the server on\n")
 
 
+def migrate(bot, update):
+	message = bot.send_message(chat_id=update.message.chat_id, text="Heading towards Unformmated")
+	subprocess.call('absolute_path/scripts/migrate.sh', shell=True)
+	bot.send_message(chat_id=update.message.chat_id, reply_to_message_id=message.message_id, text="Destination reached")
+
+
+def mount(bot, update):
+	subprocess.call('absolute_path/scripts/mount.sh', shell=True) #cd on the directory (only because there is the autofs)
+	bot.send_message(chat_id=update.message.chat_id, text="Like it was your home")
+
+
 #when /sleep
 def sleep(bot, update):
 	bot.send_message(chat_id=update.message.chat_id, text="Putting on my pajamas...")
-	subprocess.call('./scripts/sleep.sh', shell=True) #shutdown -P 0 through the script
+	subprocess.call('absolute_path/scripts/sleep.sh', shell=True) #shutdown -P 0 through the script
 
 
 #when /start
@@ -52,12 +97,12 @@ def start(bot, update):
 #when /umountall
 def umountall(bot, update):
 	bot.send_message(chat_id=update.message.chat_id, text="Looking for some mounted partitions (this may take a while)")
-	subprocess.call('./scripts/umount.sh', shell=True) #kill desired process remotely
-	pidsunf = open('./resources/mountedServers.txt').readline()
+	subprocess.call('absolute_path/scripts/umount.sh', shell=True) #kill desired process remotely
+	pidsunf = open('absolute_path/resources/mountedServers.txt').readline()
 	pids = pidsunf.split()
 	if pids[0] != 'Connection':
 		bot.send_message(chat_id=update.message.chat_id, text="The partition was found")
-		file = open('./resources/mountedServers.txt')
+		file = open('absolute_path/resources/mountedServers.txt')
 		for pid in pids:
 			file.write(pid)
 		file.close()
@@ -67,13 +112,13 @@ def umountall(bot, update):
 #when /wakeup
 def wakeup(bot, update):
 	bot.send_message(chat_id=update.message.chat_id, text="Clock is ringing...")
-	subprocess.call('./scripts/wakeup.sh', shell=True) #wakeonlan through the script
+	subprocess.call('absolute_path/scripts/wakeup.sh', shell=True) #wakeonlan through the script
 
 
 #initialization of the bot through the token pasted on the file
-TOKEN = open('./resources/token.txt').read().strip()
+TOKEN = open('absolute_path/resources/token.txt').read().strip()
 #load all the permited ids in the file userIds to a list
-with open('./resources/userIds.txt') as file:
+with open('absolute_path/resources/userIds.txt') as file:
 	permittedIds = [int(x) for x in file.read().split()]
 
 
@@ -89,8 +134,10 @@ dispatcher.add_handler(CommandHandler('sleep', sleep, Filters.user(user_id=permi
 dispatcher.add_handler(CommandHandler('start', start, Filters.user(user_id=permittedIds)))
 dispatcher.add_handler(CommandHandler('umountall', umountall, Filters.user(user_id=permittedIds)))
 dispatcher.add_handler(CommandHandler('wakeup', wakeup, Filters.user(user_id=permittedIds)))
-dispatcher.add_handler(CommandHandler('proves', proves, Filters.user(user_id=permittedIds)))
-
+dispatcher.add_handler(CommandHandler('proves', proves, Filters.user(user_id=permittedIds), pass_args=True))
+dispatcher.add_handler(CommandHandler('mount', mount, Filters.user(user_id=permittedIds)))
+dispatcher.add_handler(CommandHandler('list', list, Filters.user(user_id=permittedIds), pass_args=True))
+dispatcher.add_handler(CommandHandler('migrate', migrate, Filters.user(user_id=permittedIds)))
 
 #start the bot
 updater.start_polling()
