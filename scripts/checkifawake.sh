bcaa977c8f65647e22f42adfb117b246b23022ed
Miguel Sánchez Palomino <miguelsanchez1703@gmail.com>
2020-03-26 20:01:24 +0100

    Delete checkifawake.sh

--- scripts/checkifawake.sh
+++
@@ -1,6 +0,0 @@
-if ! ping -q -w3 -c 1 ip_address #ping the desired ip during 3 seconds to know the status (change to your own IP or domain)
-	then
-		echo "asleep" > absolute_path/resources/status.txt #write output in status.txt (change path to your own)
-else
-		echo "awake" > absolute_path/resources/status.txt #write output to status.txt (change path to your own)
-fi
