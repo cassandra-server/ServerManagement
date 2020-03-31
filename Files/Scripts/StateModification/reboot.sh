e605c19a6f0e814cecfa20ffd9e44f25f440fd9f
Miguel Sánchez Palomino <miguelsanchez1703@gmail.com>
2020-03-31 13:56:06 +0200

    changed && for ; and reboot output

--- Files/Scripts/StateModification/reboot.sh
+++ Files/Scripts/StateModification/reboot.sh
@@ -1,1 +1,1 @@
-ssh -t `cat $HOME/.ServerManagement/Files/Resources/Authentication/SSH/serveruser.txt`@`cat $HOME/.ServerManagement/Files/Resources/Authentication/SSH/serverip.txt` sudo reboot
+ssh -t `cat $HOME/.ServerManagement/Files/Resources/Authentication/SSH/serveruser.txt`@`cat $HOME/.ServerManagement/Files/Resources/Authentication/SSH/serverip.txt` "sudo reboot" ; sleep 10 ; while ! ping -q -c 1 `cat $HOME/.ServerManagement/Files/Resources/Authentication/SSH/serverip.txt` ; do sleep 1 ; done
