926ddb9e22a291c3ffa8091ebb19f2b2954acef7
Miguel Sánchez Palomino <miguelsanchez1703@gmail.com>
2020-04-03 16:27:48 +0200

    Removed * from directory on mv

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ mkdir $HOME/.ServerManagement
 
 wget https://github.com/MiguelSanchezP/ServerManagement/archive/master.zip -P $HOME/.ServerManagement/
 
-unzip $HOME/.ServerManagement/master.zip && mv $HOME/.ServerManagement/ServerManagement-master/* $HOME/.ServerManagement/* && rm -r $HOME/.ServerManagement/ServerManagement-master/
+unzip $HOME/.ServerManagement/master.zip && mv $HOME/.ServerManagement/ServerManagement-master/* $HOME/.ServerManagement/ && rm -r $HOME/.ServerManagement/ServerManagement-master/
