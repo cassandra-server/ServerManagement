3fbff0b5a0e5743527eec18f04cf23b1409ab66b
Miguel Sánchez Palomino <miguelsanchez1703@gmail.com>
2020-04-03 15:09:45 +0200

    Started with the setup.sh file

---
+++ setup.sh
@@ -0,0 +1,3 @@
+mkdir $HOME/.ServerManagement;
+wget https://github.com/MiguelSanchezP/ServerManagement/archive/master.zip -P $HOME/.ServerManagement/;
+unzip $HOME/.ServerManagement/master.zip && mv $HOME/.ServerManagement/ServerManagement-master/* $HOME/.ServerManagement/* $$ rm -r $HOME/.ServerManagement/ServerManagement-master/;
