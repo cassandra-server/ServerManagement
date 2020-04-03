bf5db22f7f54f7dc1a8d2f95f2b0f3cc06d89fe4
Miguel Sánchez Palomino <miguelsanchez1703@gmail.com>
2020-04-03 16:27:07 +0200

    Fixed errors

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
-mkdir $HOME/.ServerManagement;
-wget https://github.com/MiguelSanchezP/ServerManagement/archive/master.zip -P $HOME/.ServerManagement/;
-unzip $HOME/.ServerManagement/master.zip && mv $HOME/.ServerManagement/ServerManagement-master/* $HOME/.ServerManagement/* $$ rm -r $HOME/.ServerManagement/ServerManagement-master/;
+#!/bin/bash
+mkdir $HOME/.ServerManagement
+
+wget https://github.com/MiguelSanchezP/ServerManagement/archive/master.zip -P $HOME/.ServerManagement/
+
+unzip $HOME/.ServerManagement/master.zip && mv $HOME/.ServerManagement/ServerManagement-master/* $HOME/.ServerManagement/* && rm -r $HOME/.ServerManagement/ServerManagement-master/
