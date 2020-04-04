0cccbc6bb4762911c173f5c9ae5a1187eea5fe92
Miguel Sánchez Palomino <miguelsanchez1703@gmail.com>
2020-04-04 14:05:31 +0200

    All credentials input established

--- setup.sh
+++ setup.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 mkdir $HOME/.ServerManagement
-
 wget https://github.com/MiguelSanchezP/ServerManagement/archive/master.zip -P $HOME/.ServerManagement/
-
-unzip $HOME/.ServerManagement/master.zip && mv $HOME/.ServerManagement/ServerManagement-master/* $HOME/.ServerManagement/ && rm -r $HOME/.ServerManagement/ServerManagement-master/
+unzip $HOME/.ServerManagement/master.zip -d $HOME/.ServerManagement/ && mv $HOME/.ServerManagement/ServerManagement-master/* $HOME/.ServerManagement/ && rm -r $HOME/.ServerManagement/ServerManagement-master/
+rm $HOME/.ServerManagement/master.zip
+read -p "What is the Telegram Bot Token: " token
+echo $token > $HOME/.ServerManagement/Files/Resources/Authentication/Functioning/token.txt
+read -p "Write the userId of the superuser: " userid
+echo $userid > $HOME/.ServerManagement/Files/Resources/Authentication/Functioning/superuserIds.txt
+echo $userid > $HOME/.ServerManagement/Files/Resources/Authentication/Functioning/userIds.txt
+read -p "Write the name to recognize the server: " servername
+read -p "Write the username to access the machine: " serveruser
+read -p "Write the ip address of the server: " serverip
+read -p "Write the mac address of the server: " servermac
+echo $serveruser > $HOME/.ServerManagement/Files/Resources/Authentication/SSH/serveruser.txt
+echo $serverip > $HOME/.ServerManagement/Files/Resources/Authentication/SSH/serverip.txt
+echo $servermac > $HOME/.ServerManagement/Files/Resources/Authentication/SSH/servermac.txt
+echo "$servername $serveruser $serverip $servermac" > $HOME/.ServerManagement/Files/Resources/Authentication/Machines/machines.txt
